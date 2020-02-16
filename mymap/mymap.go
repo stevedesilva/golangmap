f64fc58f4c6b2090d1929e7eac1f8cf51b64c643
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2020-02-16 12:00:20 +0000

    tests

---
+++ mymap/mymap.go
@@ -0,0 +1,87 @@
+package mymap
+
+import "fmt"
+
+// // ---------------------------------------------------------
+// // EXERCISE: Warm-up
+// //
+// //  Create and print the following maps.
+// //
+// //  1. Phone numbers by last name
+// //  2. Product availability by Product ID
+// //  3. Multiple phone numbers by last name
+// //  4. Shopping basket by Customer ID
+// //
+// //     Each item in the shopping basket has a Product ID and
+// //     quantity. Through the map, you can tell:
+// //     "Mr. X has bought Y bananas"
+// //
+// // ---------------------------------------------------------
+
+// MyMaps base
+type MyMaps struct{}
+
+// New constructor
+func New() *MyMaps {
+	return &MyMaps{}
+}
+
+// NumbersMap exercise
+func (m *MyMaps) NumbersMap() map[string]string {
+
+	// Key        : Last name
+	// Element    : Phone number
+	numbers := map[string]string{
+		"max":   "123566777",
+		"bruce": "123566774",
+		"saul":  "123566776",
+		"mike":  "123566737",
+	}
+	fmt.Printf("numbers %#v \n", numbers)
+	fmt.Printf("numbers %v \n", numbers)
+	return numbers
+}
+
+// ProductMap exercise
+func (m *MyMaps) ProductMap() map[int]bool {
+
+	// Key        : Product ID
+	// Element    : Available / Unavailable
+	products := map[int]bool{
+		1: false,
+		2: true,
+	}
+	fmt.Printf("numbers %#v \n", products)
+
+	return products
+}
+
+// MultinumbersMap exercise
+func (m *MyMaps) MultinumbersMap() map[string][]string {
+
+	// #3
+	// Key        : Last name
+	// Element    : Phone numbers
+	multinumbers := map[string][]string{
+		"max":   {"123566777", "988"},
+		"bruce": {"123566777", "33"},
+		"saul":  {"123566777", "111"}}
+	fmt.Printf("numbers %#v \n", multinumbers)
+
+	return multinumbers
+}
+
+// CustomeridMap exercise
+func (m *MyMaps) CustomeridMap() map[int]map[string]int {
+	// #4
+	// Key        : Customer ID
+	// Element Key:
+	//   Key: Product ID Element: Quantity
+	customerid := map[int]map[string]int{
+		1: {"a2": 1},
+		2: {"a1": 12},
+	}
+	fmt.Printf("numbers %#v \n", customerid)
+	return customerid
+
+}
---
+++ mymap/mymap_test.go
@@ -0,0 +1,92 @@
+package mymap_test
+
+import (
+	// "fmt"
+	// "reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/stevedesilva/golangmap/mymap"
+)
+
+func TestMyMap_init_maps(t *testing.T) {
+	assert := assert.New(t)
+	m := mymap.New()
+	assert.NotNil(m)
+
+}
+
+// func TestInsertionSort_should_sort_sequence(t *testing.T) {
+// 	type test struct {
+// 		input []int
+// 		want  []int
+// 	}
+
+// 	tests := []test{
+// 		{
+// 			input: []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
+// 			want:  []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+// 		},
+// 		{
+// 			input: []int{4, 1, 3, 2, 5, 0},
+// 			want:  []int{0, 1, 2, 3, 4, 5},
+// 		},
+// 		{
+// 			input: []int{1, 9, 2, 5},
+// 			want:  []int{1, 2, 5, 9},
+// 		},
+// 	}
+
+// 	for _, tc := range tests {
+// 		actual := insertion.Sort(tc.input)
+// 		if !reflect.DeepEqual(tc.want, actual) {
+// 			t.Errorf("expected: %v, got: %v", tc.want, actual)
+// 		}
+// 	}
+
+// }
+
+// func TestMyMap_init() {
+// 	// Hint: Store phone numbers as text
+// 	m := mymap.New()
+
+// 	// #1
+// 	// Key        : Last name
+// 	// Element    : Phone number
+// 	numbers := map[string]string{
+// 		"max":   "123566777",
+// 		"bruce": "123566774",
+// 		"saul":  "123566776",
+// 		"mike":  "123566737",
+// 	}
+// 	fmt.Printf("numbers %#v \n", numbers)
+// 	fmt.Printf("numbers %v \n", numbers)
+// 	// #2
+// 	// Key        : Product ID
+// 	// Element    : Available / Unavailable
+// 	products := map[int]bool{
+// 		1: false,
+// 		2: true,
+// 	}
+// 	fmt.Printf("numbers %#v \n", products)
+
+// 	// #3
+// 	// Key        : Last name
+// 	// Element    : Phone numbers
+// 	multinumbers := map[string][]string{
+// 		"max":   {"123566777", "988"},
+// 		"bruce": {"123566777", "33"},
+// 		"saul":  {"123566777", "111"}}
+// 	fmt.Printf("numbers %#v \n", multinumbers)
+
+// 	// #4
+// 	// Key        : Customer ID
+// 	// Element Key:
+// 	//   Key: Product ID Element: Quantity
+// 	customerid := map[int]map[string]int{
+// 		1: {"a2": 1},
+// 		2: {"a1": 12},
+// 	}
+// 	fmt.Printf("numbers %#v \n", customerid)
+// }
